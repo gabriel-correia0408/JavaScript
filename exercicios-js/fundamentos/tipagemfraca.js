220115d240642e73c3a39213e40380ffd93a541d
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-03-08 22:00:45 -0300

    testando, para o js não ignorar comentarios dentro do código

--- exercicios-js/fundamentos/tipagemfraca.js
+++ exercicios-js/fundamentos/tipagemfraca.js
@@ -1,11 +1,20 @@
+// fazendo na linha a baixo atribução de uma varivél de nome "qualquer", que vai receber
+// Uma string "legal", como valor
 let qualquer = 'legal' 
 
+
+//imprimindo no console a o valor da variavél e seu tipo até aqui vai ser string
 console.log(qualquer)
 console.log(typeof qualquer)
 
 
-
+// agora pegando a mesma variavél criada nas linhas a cima e atribuindo um novo  valor a variavél
+// mas desta vez atribuindo um valor numérico
 qualquer = 3.15151
 
+
+/* imprimindo o valor que a variavél contém atualmente, e também seu novo tipo atribuindo
+*  assim notando sua tipagem fraca
+*/
 console.log(qualquer)
 console.log(typeof qualquer)
