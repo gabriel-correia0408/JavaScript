0d7ca7807b92755c3fc75b10fa747aae20625a97
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-08-16 20:15:00 -0300

    operadores aritimaticos

---
+++ exercicios-js/fundamentos/aritimeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 // resto da divisão, para saber se é par ou impar neste caso se o resultado ofr 0 é par, e se for 1 o resultado é impar.
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
