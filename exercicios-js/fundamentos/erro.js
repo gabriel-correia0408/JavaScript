3094b67be8b9db90a043b78522282b5e86c65d7b
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-08-17 21:22:42 -0300

    tratamento erro js try catch throw

---
+++ exercicios-js/fundamentos/erro.js
@@ -0,0 +1,25 @@
+function tratarErroElancar(erro){
+    //throw new Error('...Ocorreu um erro')
+    //throw 10
+    //throw true
+    //throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try{
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarErroElancar(e)
+    } finally {
+        console.log('final')
+    }
+}
+
+
+const obj = { nome: 'Roberto' }
+imprimirNomeGritado(obj)
