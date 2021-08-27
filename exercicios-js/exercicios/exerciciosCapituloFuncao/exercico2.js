07d212a8bcdb3d9a81066dccca37212e9e7312c0
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-08-26 22:38:40 -0300

    exercicio número dois do capitulo de funções js

--- exercicios-js/exercicios/exerciciosCapituloFuncao/exercico2.js
+++ exercicios-js/exercicios/exerciciosCapituloFuncao/exercico2.js
@@ -9,3 +9,19 @@ triângulo).
 */
 
 
+const calcularTipoTriangulo = function(lado1, lado2, lado3) {
+    if (lado1 == lado2 && lado1 == lado3) {
+        return console.log('O triângulo é quilatero')
+    } else if (lado1 != lado2 && lado1 == lado3 || lado1 != lado3 && lado1 == lado2 || 
+        lado2 != lado1 && lado2 == lado3 || lado2 != lado3 && lado2 == lado1 ||
+        lado3 != lado1 && lado3 == lado2 || lado3 != lado2 && lado3 == lado1   ) {
+            return console.log('O triângulo é isoceles')
+    } else{
+        return console.log('O triângulo é escalo, pois os três lados são diferentes')
+    }
+
+}
+
+calcularTipoTriangulo(ladao1 = 10, lado2 = 10, lado3 = 10)
+calcularTipoTriangulo(lado1 = 10, lado2 = 10, lado3 = 20 )
+calcularTipoTriangulo(lado1 = 10, lado2 = 20, lado3 = 30 )
