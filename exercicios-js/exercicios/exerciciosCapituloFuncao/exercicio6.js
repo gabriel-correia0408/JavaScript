4b8135945111c5db4fef2e7f705de599068118a5
Gabreil Correia <gabrielcorreia0913@gmail.com>
2021-08-29 20:52:14 -0300

    resolução de exercício 6 de capitulo função

--- exercicios-js/exercicios/exerciciosCapituloFuncao/exercicio6.js
+++ exercicios-js/exercicios/exerciciosCapituloFuncao/exercicio6.js
@@ -6,10 +6,21 @@ retornará o valor da aplicação sob o regime de juros compostos.
 
 */
 
-const jurosSimples = function() {
-
+const jurosSimples = function(capitalInical, taxaDeJuros, tempoDeAplicacao) {
+    let porcentagemJurosSimples = capitalInical * taxaDeJuros * tempoDeAplicacao
+    let valorFinalInvestimento = capitalInical + porcentagemJurosSimples
+    return valorFinalInvestimento
 }
 
-const jurosComposto = function() {
+console.log(jurosSimples(500, 0.10, 12)) // colocando 0.10 para representar a porcentagem
 
-}
+
+
+const calculoJurosComposto = function(capitalInicail, taxaDeJuros, tempo) {
+    let jurosComposto = (1 + taxaDeJuros) ** tempo
+    console.log(jurosComposto)
+    let montante = capitalInicail * jurosComposto
+    return montante
+    }
+
+console.log(calculoJurosComposto(10000, 0.15, 10))
