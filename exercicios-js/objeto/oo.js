aa4848422631a2f7d25c62d47930c7a2c6c23eb7
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-08-30 21:04:01 -0300

    Introdução a OO javascript

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+         //.........
+    }
+}
+
+objeto.processamento() //  Foco passou a ser o obejto 
+
+// Principios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. herença (prototype)
+// 4. Polimorfismo
