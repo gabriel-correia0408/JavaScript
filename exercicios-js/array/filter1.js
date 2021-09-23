0591a3b383f81d3401349137aa99a1770f6d8090
GabrielCorreia <gabrielmal38@gmail.com.br>
2021-09-22 22:59:53 -0300

    filter em js

---
+++ exercicios-js/array/filter1.js
@@ -0,0 +1,20 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true},
+    { nome: 'Ipad Pro', preco: 4199, fragil: true},
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true},
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false}
+]
+
+console.log(produtos.filter(function(p) {
+    return false
+}))
+
+
+const caro = function(produto) {
+    return produto.preco > 15
+}
+
+const fragil = produto => produto.fragil === true
+
+
+console.log(produtos.filter(caro).filter(fragil))
